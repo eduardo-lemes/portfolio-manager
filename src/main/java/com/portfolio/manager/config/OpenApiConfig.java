8efb252bfff73181a29fad0be15a4f7d43478c16
Elemes <eduardo_lsf@outlook.com>
2026-06-12 07:11:25 -0300

    feat: openapi documentation

---
+++ src/main/java/com/portfolio/manager/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.portfolio.manager.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Portfolio Manager API")
+                        .description("Sistema de gerenciamento de portfólio de projetos")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("basicAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("basic")));
+    }
+}
